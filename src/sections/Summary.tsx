07e122cc03e80ba504339ead3415699d334eba20
Ícaro Teodoro <icaro.teodoro84@gmail.com>
2024-02-10 12:02:50 -0300

    correction font first section

--- src/sections/Summary.tsx
+++ src/sections/Summary.tsx
@@ -4,8 +4,8 @@ export default function Summary() {
     return (
         <section id="sumary" className="container flex flex-col-reverse md:flex-row items-start md:items-center text-white py-20 lg:py-52">
             <div className=" w-full md:w-3/5 mt-12">
-                <h1 className="font-extrabold w-full text-4xl lg:text-8xl">Ícaro Teodoro</h1>
-                <h2 className="font-extrabold text-2xl lg:text-5xl mt-2">Desenvolvedor Fullstack</h2>
+                <h1 className="font-extrabold w-full text-4xl lg:text-8xl font-poppins">Ícaro Teodoro</h1>
+                <h2 className="font-extrabold text-2xl lg:text-5xl mt-2 font-poppins">Desenvolvedor Fullstack</h2>
                 <div className="flex space-x-5 mt-8">
                     <a target="_blank" href="https://www.linkedin.com/in/%C3%ADcaro-teodoro-520259169/" className="flex gap-2 rounded-md px-3 py-3 font-bold bg-stone-700 hover:bg-stone-800 transition-all">
                         <Linkedin className="border-2 rounded-sm"/>
